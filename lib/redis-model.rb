0e17efcc5874f30ecd889b7dc3a99b9fd437a02a
Ondrej Bartas <ondrej@bartas.cz>
2012-02-26 15:48:35 +0100

    [ADD] Redis model

---
+++ lib/redis-model.rb
@@ -0,0 +1,295 @@
+# -*- encoding : utf-8 -*-
+require 'pp'
+require 'redis'
+require 'active_support'
+require 'active_support/inflector'
+require 'active_support/inflector/inflections'
+require 'active_support/core_ext/hash/keys'
+require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/class/inheritable_attributes'
+require 'string_to_bool'
+require 'database'
+
+module RedisModel
+
+  attr_accessor :args, :error, :old_key, :conf
+  
+  def self.included(base) 
+    base.send :extend, ClassMethods         
+    base.send :include, InstanceMethods  
+  end
+  
+  module ClassMethods
+    
+    def initialize_redis_model_methods conf
+      @conf = conf
+      #take all fields and make methods for them
+      conf[:fields].each do |attr, action|
+        define_method "#{attr}" do
+          if self.args[attr] || self.args[attr] == false #== false is a fi for boolean variable
+            self.args[attr].to_s.send(action)
+          else
+            nil
+          end
+        end
+        
+        define_method "#{attr}=" do |item|
+          self.args[attr] = item
+        end
+        
+        define_method "#{attr}?" do
+          !self.args[attr].nil?
+        end
+      end  
+    end
+    
+    def conf
+      @conf
+    end
+    
+    #Generates redis key for storing object
+    def generate_key(args = {})
+      out = "#{self.name.to_s.downcase.to_sym}"
+      @conf[:redis_key].each do |key|
+        if args.has_key?(key)
+          out += ":#{args[key]}"
+        else
+          out += ":*"
+        end
+      end
+      out
+    end
+    
+    #Generates redis key for storing indexes for aliases
+    def generate_alias_key(alias_name, args = {})
+      out = "#{self.name.to_s.downcase.to_sym}:#{alias_name}"
+      @conf[:redis_aliases][alias_name.to_sym].each do |key|
+        if args.has_key?(key)
+          out += ":#{args[key]}"
+        else
+          out += ":*"
+        end
+      end
+      out
+    end
+    
+    #Validates if key by arguments is valid
+    def valid_key?(args = {})
+      full_key = true
+      @conf[:redis_key].each do |key|
+        full_key = false if !args.has_key?(key) || args[key].nil?
+      end
+      full_key
+    end
+
+    #Validates if key by alias name and arguments is valid
+    def valid_alias_key?(alias_name, args = {})
+      full_key = true
+      @conf[:redis_aliases][alias_name.to_sym].each do |key|
+        full_key = false if !args.has_key?(key) || args[key].nil?
+      end
+      full_key
+    end
+
+    #Check if key by arguments exists in db
+    def exists?(args = {})
+      Database.redis.exists(self.name.constantize.generate_key(args))
+    end
+
+    #Check if key by alias name and arguments exists in db
+    def alias_exists?(alias_name, args = {})
+      Database.redis.exists(self.name.constantize.generate_alias_key(alias_name, args))
+    end
+
+    #Wrapper around find to get all instances
+    def all
+      self.find({})
+    end
+
+    #Find method for searching in redis
+    def find(args = {})
+      args.symbolize_keys!
+      out = []
+      klass = self.name.constantize
+      
+      #is key specified directly? -> no needs of looking for other keys! -> faster
+      if klass.valid_key?(args)
+        out << klass.new(args.merge(Database.redis.hgetall(klass.generate_key(args))).merge({:old_key => klass.generate_key(args)})) if klass.exists?(args)
+      else
+        Database.redis.keys(klass.generate_key(args)).each do |key|
+          out << klass.new(args.merge(Database.redis.hgetall(key)).merge({:old_key => key}))
+        end
+      end
+      out
+    end
+
+    #Find method for searching in redis
+    def find_by_alias(alias_name, args = {})
+      args.symbolize_keys!
+      out = []
+      klass = self.name.constantize
+      
+      #is key specified directly? -> no needs of looking for other keys! -> faster
+      if klass.valid_alias_key?(alias_name, args)
+        out << klass.get_by_alias(alias_name, args) if klass.alias_exists?(alias_name, args)
+      else
+        Database.redis.keys(klass.generate_alias_key(alias_name, args)).each do |key|
+          out << klass.get_by_alias_key(key)
+        end
+      end
+      out
+    end
+
+    #fastest method to get object from redis by getting it by arguments
+    def get(args = {})
+      args.symbolize_keys!
+      klass = self.name.constantize
+      if klass.valid_key?(args)
+        klass.new(args.merge(Database.redis.hgetall(klass.generate_key(args))).merge({:old_key => klass.generate_key(args)})) if klass.exists?(args)
+      else
+        nil
+      end
+    end    
+
+    #fastest method to get object from redis by getting it by alias and arguments
+    def get_by_alias(alias_name, args = {})
+      args.symbolize_keys!
+      klass = self.name.constantize
+      if klass.valid_alias_key?(alias_name, args) && klass.alias_exists?(alias_name, args)
+        key = Database.redis.get(klass.generate_alias_key(alias_name, args))
+        if Database.redis.exists(key)
+          klass.new(args.merge(Database.redis.hgetall(key)).merge({:old_key => key})) 
+        else
+          nil
+        end
+      else
+        nil
+      end
+    end    
+
+    #fastest method to get object from redis by getting it by alias and arguments
+    def get_by_alias_key(alias_key)
+      klass = self.name.constantize
+      if Database.redis.exists(alias_key)
+        key = Database.redis.get(alias_key)
+        if Database.redis.exists(key)
+          klass.new(args.merge(Database.redis.hgetall(key)).merge({:old_key => key}))
+        else
+          nil
+        end
+      else
+        nil
+      end
+    end    
+
+  end
+    
+  module InstanceMethods
+    
+    def initialize(args={})
+      args.symbolize_keys!
+      #if old_key is specified, don't usi it in args hash
+      if args[:old_key] && args[:old_key].size > 0 
+        self.old_key = args[:old_key] 
+        args.delete(:old_key)
+      end
+      self.args = clear_args(args)
+      return self
+    end
+    
+    #Fixing some problems with saving nil into redis and clearing input arguments
+    def clear_args(args)
+      args.symbolize_keys!
+      out_args = {}
+      args.each do |key, value|
+        if self.class.conf[:fields].keys.include?(key) #don't use arguments wich aren't specified in :fields 
+          if value.nil? || value.to_s.size == 0 #change nil and zero length string into nil
+            out_args[key] = nil 
+          else
+            out_args[key] = value
+          end
+        end
+      end
+      out_args
+    end
+    
+    #validates required attributes
+    def valid?
+      @error = []
+      self.class.conf[:required].each do |key|
+        if !self.args.has_key?(key) || self.args[key].nil?
+          @error.push("Required #{key}")
+        end
+      end
+      @error.size == 0
+    end
+  
+    #return error from validation
+    def errors 
+      @errors
+    end
+  
+    #take all arguments and send them out
+    def to_arg
+      self.args
+    end
+  
+    #if this record exists in database
+    def exists?
+      Database.redis.exists(self.class.generate_key(self.args))
+    end
+
+    #remove record form database
+    def destroy!
+      if exists?
+        #destroy main object
+        Database.redis.del(generate_key) 
+        destroy_aliases!
+      end
+    end
+
+    #remove all aliases
+    def destroy_aliases!
+      self.class.conf[:redis_aliases].each do |alias_name, fields|
+        Database.redis.del(self.class.generate_alias_key(alias_name, self.args)) if self.class.alias_exists?(alias_name, self.args)
+      end
+    end
+    
+    #Method for creating aliases
+    def create_aliases
+      main_key = redis_key
+      self.class.conf[:redis_aliases].each do |alias_name, fields|
+        Database.redis.set(self.class.generate_alias_key(alias_name, self.args), main_key)
+      end
+    end
+  
+    #get redis key for instance
+    def redis_key
+      self.class.generate_key(self.args)
+    end
+    
+    #get redis key for instance alias
+    def redis_alias_key(alias_name)
+      self.class.generate_alias_key(alias_name, self.args)
+    end
+  
+    #save method
+    def save
+      if valid?
+        #generate key (possibly new)
+        generated_key = redis_key
+        Database.redis.rename(self.old_key, generated_key) if self.old_key && generated_key != self.old_key
+        Database.redis.hmset(generated_key, *self.args.reject{|k,v| v.nil?}.inject([]){ |arr,kv| arr + [kv[0], kv[1].to_s]})
+        
+        #destroy aliases
+        destroy_aliases!
+        create_aliases
+        #after save make new_key -> old_key
+        self.old_key = generated_key  
+        return self
+      else
+        raise ArgumentError, @error.join(", ")
+      end
+    end
+  end
+end
--- test/test_redis-model.rb
+++ test/test_redis-model.rb
@@ -1,7 +1,123 @@
+# -*- encoding : utf-8 -*-
 require 'helper'
+class RedisModelTest < Test::Unit::TestCase
+  context "RedisModel" do
+    setup do
+      Database.redis.flushdb
+      
+      class TestRedisModel
+        REDIS_MODEL_CONF = {
+           :fields => { 
+             :integer => :to_i,
+             :boolean => :to_bool,
+             :string => :to_s,
+             :symbol => :to_sym,
+            }, 
+            :required => [:integer, :string],
+            :redis_key => [:string],
+            :redis_aliases => {
+              :token => [:symbol]
+            }
+         }
+         include RedisModel
+         initialize_redis_model_methods REDIS_MODEL_CONF
+      end
+      @args = {:integer => 12345, :string => "foo", :symbol => :bar, :boolean => true}
+      @test_model = TestRedisModel.new(@args)
+      @test_model_partial = TestRedisModel.new(:integer => 12345, :string => "foo")
+    end 
+    
+    context "define methods" do
+      should "be accessible" do
+        assert @test_model.respond_to?(:integer)
+        assert @test_model.respond_to?(:boolean)
+        assert @test_model.respond_to?(:string)
+        assert @test_model.respond_to?(:symbol)
+      end
+      
+      should "get valid arguments" do
+        assert_equal @test_model.integer, 12345
+        assert_equal @test_model.string, "foo"
+        assert_equal @test_model.symbol, :bar
+        assert_equal @test_model.boolean, true
+      end
+      
+      should "return valid exists?" do
+        assert_equal @test_model.integer?, true
+        assert_equal @test_model.string?, true
+        assert_equal @test_model.symbol?, true
+        assert_equal @test_model.boolean?, true
+        
+        assert_equal @test_model_partial.integer?, true
+        assert_equal @test_model_partial.string?, true
+        assert_equal @test_model_partial.symbol?, false
+        assert_equal @test_model_partial.boolean?, false
+      end
+      
+      should "be assign new values" do
+        @test_model.integer = 54321
+        @test_model.string = "bar"
+        @test_model.symbol = :foo
+        @test_model.boolean = false
+        assert_equal @test_model.integer, 54321
+        assert_equal @test_model.string, "bar"
+        assert_equal @test_model.symbol, :foo
+        assert_equal @test_model.boolean, false
+      end
+    end
+         
+    context "redis key" do
+      should "generate right key" do
+        assert_equal @test_model.redis_key, "redismodeltest::testredismodel:foo"
+        assert_equal TestRedisModel.generate_key(@args), "redismodeltest::testredismodel:foo"
+      end  
+      should "generate right key alias" do
+        assert_equal @test_model.redis_alias_key(:token), "redismodeltest::testredismodel:token:bar"
+        assert_equal TestRedisModel.generate_alias_key(:token, @args), "redismodeltest::testredismodel:token:bar"
+      end  
+    end
+    
+    context "after initialize" do 
+      should "clear input arguments" do
+        test_model = TestRedisModel.new(@args.merge({:foor => :bar, :not_in_fields => "foo"}))
+        assert_same_elements test_model.args, @args
+      end
+    end
+    
+    context "validation" do
+      should "not raise exeption on invalid initialize" do
+        assert_nothing_raised { TestRedisModel.new() }
+      end
 
-class TestRedisModel < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+      should "raise exeption on save" do
+        test_model = TestRedisModel.new()
+        assert_raises ArgumentError do
+          test_model.save
+        end
+      end
+    end
+    
+    context "saving" do
+      setup do
+        @test_model.save
+      end
+      
+      should "have same elements after get" do
+        @getted_model = TestRedisModel.get(@args)
+        assert_equal @getted_model.integer, @test_model.integer
+        assert_equal @getted_model.string, @test_model.string
+        assert_equal @getted_model.symbol, @test_model.symbol
+        assert_equal @getted_model.boolean, @test_model.boolean
+      end
+      
+      should "be getted by alias" do
+        @getted_model = TestRedisModel.get_by_alias(:token ,@args)
+        assert_equal @getted_model.integer, @test_model.integer
+        assert_equal @getted_model.string, @test_model.string
+        assert_equal @getted_model.symbol, @test_model.symbol
+        assert_equal @getted_model.boolean, @test_model.boolean
+      end
+    end
+    
   end
 end
