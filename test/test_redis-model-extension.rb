78e755b84abfbedd90bbcfa1162a754909fbde27
Ondrej Bartas <ondrej@bartas.cz>
2012-08-11 10:31:37 +0200

    [ADD] Test for save nil values for old and new initialization method

--- test/test_redis-model-extension.rb
+++ test/test_redis-model-extension.rb
@@ -5,7 +5,7 @@ class RedisModelTest < Test::Unit::TestCase
 
     context "without rejected nil values on save" do
       should "save nil values" do
-        class NilTestRedisModel
+        class NilTestOldRedisModel
           REDIS_MODEL_CONF = {
              :fields => { 
                :integer => :to_i,
@@ -15,34 +15,48 @@ class RedisModelTest < Test::Unit::TestCase
               :redis_key => [:string],
               :redis_aliases => {},
               :reject_nil_values => false
-           }
-           include RedisModel
-           initialize_redis_model_methods REDIS_MODEL_CONF
+          }
+          include RedisModel
+          initialize_redis_model_methods REDIS_MODEL_CONF
         end
-        args = {integer: 100, string: "test"}
-        nil_test_model = NilTestRedisModel.new(args)
 
-        #on initialize
-        assert_equal nil_test_model.integer, 100
-        assert_equal nil_test_model.string, "test"
-        nil_test_model.save
+        class NilTestRedisModel
+          include RedisModelExtension
+          redis_field :integer, :integer
+          redis_field :string, :string
+          redis_validate :string
+          redis_key :string
+          redis_save_fields_with_nil true
+        end
 
-        #after find
-        founded = NilTestRedisModel.get(args)
-        assert_equal founded.integer, 100
-        assert_equal founded.string, "test"
+        [NilTestOldRedisModel, NilTestRedisModel].each do |klass|
+          args = {integer: 100, string: "test"}
+          nil_test_model = klass.new(args)
 
-        #set integer to nil
-        founded.integer = nil
-        assert_equal founded.integer, nil
-        #perform save
-        founded.save
+          #on initialize
+          assert_equal nil_test_model.integer, 100, "For #{klass} should have integer = 100"
+          assert_equal nil_test_model.string, "test", "For #{klass} should have string = test"
+          nil_test_model.save
 
-        #after second find
-        founded = NilTestRedisModel.get(args)
-        assert_equal founded.integer, nil
-        assert_equal founded.string, "test"
+          #after find
+          founded = klass.get(args)
+          assert_equal founded.integer, 100, "After get for #{klass} should have integer = 100"
+          assert_equal founded.string, "test", "After get for #{klass} should have string = test"
+
+          #set integer to nil
+          founded.integer = nil
+          assert_equal founded.integer, nil, "After get and save for #{klass} should have integer = nil"
+          #perform save
+          founded.save
+
+          #after second find
+          founded = klass.get(args)
+          assert_equal founded.integer, nil, "After 2nd get for #{klass} should have integer = nil"
+          assert_equal founded.string, "test", "After 2nd get for #{klass} should have string = test"
+        end
+
       end
     end
+    
   end
 end
